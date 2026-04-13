d9e7db4c24c228174ab8ff9aff1e96c212630278
LeJuanChis <juancaom12@gmail.com>
2026-04-13 18:15:57 -0500

    feat: agregar nuevos componentes y ajustar home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/pages/home/home';
+import { ServiceDetail } from './features/service-detail/pages/service-detail/service-detail';
+import { ServiceList } from './features/service-list/pages/service-list/service-list';
+import { Contact } from './features/contact/pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Inicio' },
+  { path: 'service-detail', component: ServiceDetail, title: 'Detalle del servicio' },
+  { path: 'service-list', component: ServiceList, title: 'Lista de servicios' },
+  { path: 'contact', component: Contact, title: 'Contacto' },
+  { path: '**', redirectTo: '' }
+
+];
